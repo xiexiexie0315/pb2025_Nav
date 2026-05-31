84a401af45ce3aedad7434f27e5a4513507af4d7
Xxx <GitHub@example.com>
2026-05-31 15:54:47 +0800

    停止追踪编译产物 build/ 和 log/

--- build/auto_aim_interfaces/auto_aim_interfaces__rs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xxx/HUN_sentry_xxx/pb2025_Nav/build/auto_aim_interfaces/auto_aim_interfaces__rs
-# Build directory: /home/xxx/HUN_sentry_xxx/pb2025_Nav/build/auto_aim_interfaces/auto_aim_interfaces__rs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/nav_bridge/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/xxx/HUN_sentry_xxx/pb2025_Nav/install/auto_aim_interfaces/share/auto_aim_interfaces/package.sh"
